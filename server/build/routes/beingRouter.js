bcc7c276b2e9fec0405bf5a67f010a982f06b336
Luka Cadjenovic <lukacadjenovic24@gmail.com>
2022-11-22 22:33:44 +0100

    Deleting Vercel stuff, switched to Render

--- server/build/routes/beingRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const express = require("express");
-const beingController = require("../controllers/beingController");
-const router = express.Router();
-router.route("/").get(beingController.getAll);
-router.route("/name").get(beingController.getByName);
-module.exports = router;
